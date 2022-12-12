5491eca73e6bb2b163f7dccdde3c1979db4d6c1a
Michael Kiros <michael.kiros00@gmail.com>
2022-12-12 12:57:05 -0500

    commit tree 9059f43781145df071a6e47304ca0fd07adf866e

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// code your solution here
+
+function superbowlWin(record) {
+    for (const key of record){
+        if (key["result"]==='W'){
+            return key["year"]
+        }
+    }
+}
+record.isFind(superbowlWin)
